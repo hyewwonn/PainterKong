931b7e0ca813b32caa9bc8579934df826eb012a0
hyewwonn <w2106@e-mirim.hs.kr>
2022-11-23 13:15:40 +0900

    meal

--- js/meal.js
+++ js/meal.js
@@ -6,12 +6,12 @@ const day = date.getDate();
 const hour = date.getHours();
 const minutes = date.getMinutes();
 
-console.log('date: ' + date.toLocaleDateString('ko-kr'));
-console.log('year: ' + year);
-console.log('month: ' + month);
-console.log('day: ' + day);
-
+// console.log('date: ' + date.toLocaleDateString('ko-kr'));
+// console.log('year: ' + year);
+// console.log('month: ' + month);
+// console.log('day: ' + day);
 
 const $el = document.querySelector('.user-time')
 
 $el.innerHTML += `<p id="meal-time-p">${year}년 ${month}월 ${day}일 ${hour}:${minutes}</p>`;
+
